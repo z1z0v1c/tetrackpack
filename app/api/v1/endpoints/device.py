d9bc73d640b882cb864d3668588c6c0d43cea026
Aleksandar Zizovic <42997751+z1z0v1c@users.noreply.github.com>
2025-09-07 21:53:14 +0200

    Add get device API endpoint

--- app/api/v1/endpoints/device.py
+++ app/api/v1/endpoints/device.py
@@ -1,8 +1,8 @@
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session
 
-from app.database.device import create_device, read_devices
+from app.database.device import create_device, read_device, read_devices
 from app.database.session import get_db
 from app.schemas.device import DeviceCreate, DeviceResponse
 
@@ -19,3 +19,11 @@ async def post_device(device: DeviceCreate, db: Session = Depends(get_db)):
 async def get_devices(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     devices = read_devices(db, skip=skip, limit=limit)
     return devices
+
+
+@router.get("/{device_id}", response_model=DeviceResponse)
+async def get_device(device_id: int, db: Session = Depends(get_db)):
+    device = read_device(db, device_id)
+    if not device:
+        raise HTTPException(status_code=404, detail="Device not found")
+    return device
--- app/database/device.py
+++ app/database/device.py
@@ -13,5 +13,10 @@ def create_device(db: Session, device: DeviceCreate):
 
     return db_device
 
+
 def read_devices(db: Session, skip: int = 0, limit: int = 100):
     return db.exec(select(Device).offset(skip).limit(limit)).all()
+
+
+def read_device(db: Session, device_id: int):
+    return db.exec(select(Device).where(Device.id == device_id)).first()
