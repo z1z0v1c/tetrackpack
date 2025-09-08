be73fe23ec113e02b9bb387a341e7989c343870f
Aleksandar Zizovic <42997751+z1z0v1c@users.noreply.github.com>
2025-09-08 17:22:12 +0200

    Add PUT rack API endpoint

--- app/api/v1/endpoints/rack.py
+++ app/api/v1/endpoints/rack.py
@@ -2,9 +2,9 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session
 
-from app.database.rack import create_rack, read_rack, read_racks
+from app.database.rack import create_rack, read_rack, read_racks, update_rack
 from app.database.session import get_db
-from app.schemas.rack import RackResponse, RackCreate
+from app.schemas.rack import RackResponse, RackCreate, RackUpdate
 
 
 router = APIRouter()
@@ -17,8 +17,7 @@ def post_rack(rack: RackCreate, db: Session = Depends(get_db)):
 
 @router.get("/", response_model=List[RackResponse])
 def get_racks(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    racks = read_racks(db, skip=skip, limit=limit)
-    return racks
+    return read_racks(db, skip=skip, limit=limit)
 
 
 @router.get("/{rack_id}", response_model=RackResponse)
@@ -28,3 +27,12 @@ def get_rack(rack_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Rack not found")
     
     return rack
+
+
+@router.put("/{rack_id}", response_model=RackResponse)
+def put_rack(rack_id: int, rack: RackUpdate, db: Session = Depends(get_db)):
+    db_rack = update_rack(db, rack_id, rack)
+    if not db_rack:
+        raise HTTPException(status_code=404, detail="Rack not found")
+    
+    return db_rack
--- app/database/rack.py
+++ app/database/rack.py
@@ -1,6 +1,6 @@
 from sqlmodel import Session, select
 from app.models.rack import Rack
-from app.schemas.rack import RackCreate
+from app.schemas.rack import RackCreate, RackUpdate
 
 
 def create_rack(db: Session, rack: RackCreate):
@@ -19,3 +19,19 @@ def read_racks(db: Session, skip: int = 0, limit: int = 100):
 
 def read_rack(db: Session, rack_id: int):
     return db.exec(select(Rack).where(Rack.id == rack_id)).first()
+
+
+def update_rack(db: Session, rack_id: int, rack: RackUpdate):
+    db_rack = read_rack(db, rack_id)
+    if not db_rack:
+        return None
+    
+    rack_data = rack.dict(exclude_unset=True)
+    for key, value in rack_data.items():
+        setattr(db_rack, key, value)
+    
+    db.add(db_rack)
+    db.commit()
+    db.refresh(db_rack)
+    
+    return db_rack
